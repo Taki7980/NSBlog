4bdc7dd960c4b2172667a28eadf06f0c0be7f1de
nirjhar sarkar <nirjharsarkar8@gmail.com>
2024-01-31 19:53:46 +0530

    added and correced some tailwind components

--- src/components/reuseable/Heading.tsx
+++ src/components/reuseable/Heading.tsx
@@ -7,18 +7,33 @@ interface HeadingProps {
 	subtitle?: string;
 	center?: boolean;
 	para?: string;
+	classN?: string;
 }
 
-const Heading: React.FC<HeadingProps> = ({ title, subtitle, center, para }) => {
+const Heading: React.FC<HeadingProps> = ({
+	title,
+	subtitle,
+	center,
+	para,
+	classN,
+}) => {
 	return (
 		<div
 			className={cn(
-				`${center ? "text-center items-center" : "text-start"} h-full flex flex-col  justify-center w-full pr-5`
+				`${
+					center ? "text-center items-center" : "text-start"
+				} h-full flex flex-col gap-5 md:gap-0 justify-between w-full pr-5`,
+				classN
 			)}
 		>
-			<div className="text-5xl font-bold">{title}</div>
-			<div className="font-medium text-3xl text-neutral-600 mt-2">{subtitle}</div>
-			<div className="font-light text-neutral-500 mt-2">{para}</div>
+			
+			<div className="flex flex-col gap-3 justify-between">
+				<div className="font-medium text-2xl text-violet-600">
+					{subtitle}
+				</div>
+				<div className="text-5xl font-bold">{title}</div>
+			</div>
+			<div className="font-light ">{para}</div>
 		</div>
 	);
 };
